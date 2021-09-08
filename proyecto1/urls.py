80525916184a11871b9cfe7affa1c644b4e74ded
omar <reissing@yahoo.com>
2021-09-08 12:07:06 -0300

    Tercer actualizacion

--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from django.conf.urls import url
+from blog import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', views.post_list),
 ]
